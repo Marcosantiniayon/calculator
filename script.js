5c9d5f87096337a7d13d649731d29fae5053c3ca
Marco S <marcosantinia12@gmail.com>
2022-08-08 20:29:31 -0700

    Created conditions for buttons and gave clear & back buttons functionality

--- script.js
+++ script.js
@@ -1,59 +1,173 @@
-const operations = document.querySelector(".operations");
-const result = document.querySelector(".result");
+const operationsDisplay = document.querySelector(".operations");
+const resultDisplay = document.querySelector(".result");
 const buttons = document.querySelectorAll("button");
-let input = "";
-let newInput = ""
-// let Btn1 = document.querySelector("#1");
-// let Btn2 = document.querySelector("#2");
-// let Btn3 = document.querySelector("#3");
-// let Btn4 = document.querySelector("#4");
-// let Btn5 = document.querySelector("#5");
-// let Btn6 = document.querySelector("#6");
-// let Btn7 = document.querySelector("#7");
-// let Btn8 = document.querySelector("#8");
-// let Btn9 = document.querySelector("#9");
-// let Btn0 = document.querySelector("#0");
-// let BtnPer = document.querySelector("#.");
-// let BtnDiv = document.querySelector("#1");
-// let BtnMult = document.querySelector("#1");
-// let BtnSub = document.querySelector("#-");
-// let BtnAdd = document.querySelector("#+");
-// let BtnEqu = document.querySelector("#=");
+let firstOperand = "";
+let secondOperand = "";
+let operation = false;
+let operation2 = false;
+let currentOperand = "";
+let operator = "";
+let operator2 = "";
+let total = 0;
+let finished = false;
+//let input = "";
 
 
-
-
-
-operations.innerHTML = "";
-result.innerHTML = "result";
+operationsDisplay.innerHTML = "";
+resultDisplay.innerHTML = "result";
 
 buttons.forEach(button => button.addEventListener('click', collectData))
-function collectData(){
-    if(this.id === "="){
-        input = input + this.innerHTML;;
-        console.log(input);
-        operations.innerHTML = input;
-    }else if(this.id === "clear"){
-        input = "";
-        console.log(input);
-        operations.innerHTML = input;
-    }else if(this.id === "0"){
-        if(input === ""){
-            input = "";
-        } else {
-            input = input + this.innerHTML;
-            console.log(input);
-            operations.innerHTML = input;
-        }
-    }else if(this.id === "back"){
-        input = input.slice(0,-1);
-        console.log(input);
-        operations.innerHTML = input;
-    }else {
-        input = input + this.innerHTML;
-        console.log(input);
-        operations.innerHTML = input;
 
-    }
+function collectData(){
+    if(this.className === "numberBtn"){
+        if (finished === true){
+            clear();
+        }
+        if(operation2 === true){
+            console.log("Second operation");
+        } else if(operation === true){
+            console.log("First operation");
+            secondOperand += this.innerHTML;
+            console.log(firstOperand);
+            operationsDisplay.innerHTML = firstOperand + " " + operator + " " + secondOperand;;
+        } else if(operation === false && operation2 === false){
+            if(finished === true){
+                firstOperand = "";
+            }
+            console.log("No operation");
+            firstOperand += this.innerHTML;
+            console.log(firstOperand);
+            operationsDisplay.innerHTML = firstOperand + " " + operator + " ";
+        }
+    } else if(this.className === "operatorBtn"){
+        finished = false;
+        if(operation === false){
+            if(this.innerHTML != "="){
+                operation = true;
+            } else{
+                return;
+            }
+        } else {
+            operation2 = true;
+        }
+        if(operation2 === true){
+            console.log("Second operation");
+            // Calculate first operation
+            if(operator === "x"){
+                operator = this.innerHTML;
+                total = Number(firstOperand) * Number(secondOperand);
+                firstOperand = total.toString();
+                secondOperand = "";
+                operation2 = false;
+                if(operator === "="){
+                    operationsDisplay.innerHTML = total;
+                    operator = "=";
+                    operation = false;
+                    finished = true;
+                } else{
+                    operationsDisplay.innerHTML = firstOperand + " " + operator;
+                }
+            }else if(operator === "/"){
+                operator = this.innerHTML;
+                total = Math.round((Number(firstOperand) / Number(secondOperand))*1000)/1000;
+                firstOperand = total.toString();
+                secondOperand = "";
+                operation2 = false;
+                if(operator === "="){
+                    operationsDisplay.innerHTML = total;
+                    operator = "=";
+                    operation = false;
+                    finished = true;
+                } else{
+                    operationsDisplay.innerHTML = firstOperand + " " + operator;
+                }            
+            }else if(operator === "-"){
+                operator = this.innerHTML;
+                total = Number(firstOperand) - Number(secondOperand);
+                firstOperand = total.toString();
+                secondOperand = "";
+                operation2 = false;
+                if(operator === "="){
+                    operationsDisplay.innerHTML = total;
+                    operator = "=";
+                    operation = false;
+                    finished = true;
+                } else{
+                    operationsDisplay.innerHTML = firstOperand + " " + operator;
+                }            
+            }else if(operator === "+"){
+                operator = this.innerHTML;
+                total = Number(firstOperand) + Number(secondOperand);
+                firstOperand = total.toString();
+                secondOperand = "";
+                operation2 = false;
+                if(operator === "="){
+                    operationsDisplay.innerHTML = total;
+                    operator = "=";
+                    operation = false;
+                    finished = true;
+                } else{
+                    operationsDisplay.innerHTML = firstOperand + " " + operator;
+                }            
+            }
+        } else if(operation === true && operation2 === false){
+            console.log("First operation");
+            operator = this.innerHTML;
+            console.log(operator);
+            if(operator === "=")
+            {
+                operationsDisplay.innerHTML = firstOperand + " " + operator + " ";
+                finished = true;
+            }else {
+                operationsDisplay.innerHTML = firstOperand + " " + operator + " ";
+            } 
+        }
+    } else if(this.className === "clearBtn") {
+        clear();
+    } else if(this.className === "deleteBtn"){
+
+        //If is on 1st operand
+        if(firstOperand != "" && operation === false){
+            console.log("1st operand");
+            let array = {}
+            array = firstOperand.split('');
+            array.pop();
+            firstOperand = array.join('');
+            operationsDisplay.innerHTML = firstOperand;
+        }
+        //If is on operator
+        else if(operation === true && secondOperand === ""){
+            console.log("operator");
+            let array = {}
+            array = operationsDisplay.innerHTML.split('');
+            array.pop();
+            array.pop();
+            array.pop();
+            firstOperand = array.join('');
+            operator = "";
+            operation = false;
+            operationsDisplay.innerHTML = firstOperand;
+
+        }
+        //If is on 2nd operand
+        else if(secondOperand !== ""){
+            console.log("2nd operand");
+        }else {
+            return;
+        }
+    };
+
+function clear(){
+    firstOperand = "";
+    secondOperand = "";
+    operation = false;
+    operation2 = false;
+    currentOperand = "";
+    operator = "";
+    operator2 = "";
+    total = 0;
+    operationsDisplay.innerHTML = "";
+    finished = false;
+    };
 }
 
